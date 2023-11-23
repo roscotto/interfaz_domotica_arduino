f48b50eb15a5f230b96354d2cd7d77ce87b38577
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-11-23 15:31:19 -0300

    muchos cambios

---
+++ js/main.js
@@ -0,0 +1,71 @@
+function medirTemperatura() {
+  let tempDisplay = document.getElementById('temperaturaVisor')
+
+  fetch(`http://localhost:2223/medir/temperatura`)
+    .then(respuesta => {
+      if (!respuesta.ok){
+        throw new Error ('Algo salio mal')
+      }
+      return respuesta.json()    
+    })
+    .then(data => {
+      tempDisplay.innerText = `${data.temperatura}`
+    })
+    .catch( err => {
+      alert(err)
+    })
+}
+
+function medirHumedad() {
+  let humedadDisplay = document.getElementById('humedadVisor')
+
+  fetch(`http://localhost:2223/medir/humedad`)
+    .then(respuesta => {
+      if (!respuesta.ok){
+        throw new Error ('Algo salio mal')
+      }
+      return respuesta.json()    
+    })
+    .then(data => {
+      humedadDisplay.innerText = `${data.humedad}`
+    })
+    .catch( err => {
+      alert(err)
+    })
+}
+
+
+function encenderLuces() {
+  let onOffCheckbox = document.getElementById('onOffCheckbox')
+  let imgFoco = document.getElementById('imgFoco')
+  if (onOffCheckbox.checked){
+    fetch(`http://localhost:2223/encender`)
+    .then(respuesta => {
+      if (!respuesta.ok){
+        throw new Error ('Algo salio mal')
+      }
+      imgFoco.style.filter = 'brightness(1)'
+    })
+    .catch( err => {
+      alert(err)
+    })
+  }else{
+    fetch(`http://localhost:2223/apagar`)
+    .then(respuesta => {
+      if (!respuesta.ok){
+        throw new Error ('Algo salio mal')
+      }
+      imgFoco.style.filter = 'brightness(0.5)'
+    })
+    .catch( err => {
+      alert(err)
+    })
+  }
+  
+}
+
+function encenderRegador() {
+  let regaderaCheckbox = document.getElementById('regaderaCheckbox')
+  let imgPlanta = document.getElementById('imgPlanta')
+  alert('vas a encneder el regador')
+}
